72e48def67f5c8d2c68c922490e792671da753ab
irshad k <99166196+Irshad-konnola@users.noreply.github.com>
2023-03-30 15:27:44 +0530

    Add files via upload

---
+++ frontend/src/Page/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='m-10 '>
+      <div className='border-orange-900 border-2 items-center w-full p-3  '>
+        <h2 className='font-semibold text-2xl '>Created by</h2>
+        <h4>Irshad k</h4>
+        <h4>Email : irshadkmpm656@gmail.com</h4>
+        <h4>Mobile : 9072239599</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
